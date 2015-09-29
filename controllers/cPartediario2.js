558a8b73e4417fbaa45d4215313a781658109ddb
Marcos Iglesias <Markkos89@gmail.com>
2015-09-29 18:35:35 -0300

    Division total de hrs en hrs normales, 50 y 100. This closes #56

--- controllers/cPartediario2.js
+++ controllers/cPartediario2.js
@@ -135,6 +135,15 @@ function postModificar(req, res){
 	entrada = params.entrada;
 	salida = params.salida;
 	total = params.total;
+	hr_total_n = params.hr_total_n;
+	if (!hr_total_n)
+		hr_total_n = 0;
+	hr_total_50 = params.hr_total_50;
+	if (!hr_total_50)
+		hr_total_50 = 0;
+	hr_total_100 = params.hr_total_100;
+	if (!hr_total_100)
+		hr_total_100 = 0;
 	//nuevos campos
 	adicional1_n = params.adicional1_n;
 	adicional1_50 = params.adicional1_50;
@@ -300,7 +309,7 @@ function postModificar(req, res){
 	if (!item12_100)
 		item12_100 = 0;
 
-	mPartediario2.update(id, codigohora, entrada, salida, total, adicional1_n, adicional1_50, adicional1_100, adicional2_n, adicional2_50, adicional2_100, adicional3_n, adicional3_50, adicional3_100, adicional4_n, adicional4_50, adicional4_100, adicional5_n, adicional5_50, adicional5_100, adicional6_n, adicional6_50, adicional6_100, item1_n, item1_50, item1_100, item2_n, item2_50, item2_100, item3_n, item3_50, item3_100, item4_n, item4_50, item4_100, item5_n, item5_50, item5_100, item6_n, item6_50, item6_100, item7_n, item7_50, item7_100, item8_n, item8_50, item8_100, item9_n, item9_50, item9_100, item10_n, item10_50, item10_100, item11_n, item11_50, item11_100, item12_n, item12_50, item12_100, function(){
+	mPartediario2.update(id, codigohora, entrada, salida, total, hr_total_n, hr_total_50, hr_total_100, adicional1_n, adicional1_50, adicional1_100, adicional2_n, adicional2_50, adicional2_100, adicional3_n, adicional3_50, adicional3_100, adicional4_n, adicional4_50, adicional4_100, adicional5_n, adicional5_50, adicional5_100, adicional6_n, adicional6_50, adicional6_100, item1_n, item1_50, item1_100, item2_n, item2_50, item2_100, item3_n, item3_50, item3_100, item4_n, item4_50, item4_100, item5_n, item5_50, item5_100, item6_n, item6_50, item6_100, item7_n, item7_50, item7_100, item8_n, item8_50, item8_100, item9_n, item9_50, item9_100, item10_n, item10_50, item10_100, item11_n, item11_50, item11_100, item12_n, item12_50, item12_100, function(){
 		res.redirect('partediario2lista/'+idpartediario1);
 	});
 }
--- models/mPartediario2.js
+++ models/mPartediario2.js
@@ -47,9 +47,10 @@ function insertNewEmpleado(idpartediario1, idempleado, numero, cb){
 		"values("+idpartediario1+", "+idempleado+", "+numero+")", cb);
 }
 
-function update(id, codigohora, entrada, salida, total, adicional1_n, adicional1_50, adicional1_100, adicional2_n, adicional2_50, adicional2_100, adicional3_n, adicional3_50, adicional3_100, adicional4_n, adicional4_50, adicional4_100, adicional5_n, adicional5_50, adicional5_100, adicional6_n, adicional6_50, adicional6_100, item1_n, item1_50, item1_100, item2_n, item2_50, item2_100, item3_n, item3_50, item3_100, item4_n, item4_50, item4_100, item5_n, item5_50, item5_100, item6_n, item6_50, item6_100, item7_n, item7_50, item7_100, item8_n, item8_50, item8_100, item9_n, item9_50, item9_100, item10_n, item10_50, item10_100, item11_n, item11_50, item11_100, item12_n, item12_50, item12_100, cb){
+function update(id, codigohora, entrada, salida, total, hr_total_n, hr_total_50, hr_total_100, adicional1_n, adicional1_50, adicional1_100, adicional2_n, adicional2_50, adicional2_100, adicional3_n, adicional3_50, adicional3_100, adicional4_n, adicional4_50, adicional4_100, adicional5_n, adicional5_50, adicional5_100, adicional6_n, adicional6_50, adicional6_100, item1_n, item1_50, item1_100, item2_n, item2_50, item2_100, item3_n, item3_50, item3_100, item4_n, item4_50, item4_100, item5_n, item5_50, item5_100, item6_n, item6_50, item6_100, item7_n, item7_50, item7_100, item8_n, item8_50, item8_100, item9_n, item9_50, item9_100, item10_n, item10_50, item10_100, item11_n, item11_50, item11_100, item12_n, item12_50, item12_100, cb){
 	conn("UPDATE `partediario2` SET `id_codigohora_fk`="+codigohora+", `hr_entrada`='"+entrada+"', `hr_salida`='"+salida+"', "+
-		"`hr_total`='"+total+"', `adicional1_n`="+adicional1_n+",`adicional1_50`="+adicional1_50+", `adicional1_100`="+adicional1_100+", "+
+		"`hr_total`='"+total+"', hr_total_n = '"+hr_total_n+"', hr_total_50 = '"+hr_total_50+"', hr_total_100 = '"+hr_total_100+"',	"+
+		"`adicional1_n`="+adicional1_n+",`adicional1_50`="+adicional1_50+", `adicional1_100`="+adicional1_100+", "+
 		"`adicional2_n`="+adicional2_n+",`adicional2_50`="+adicional2_50+", `adicional2_100`="+adicional2_100+", "+
 		"`adicional3_n`="+adicional3_n+",`adicional3_50`="+adicional3_50+", `adicional3_100`="+adicional3_100+", "+
 		"`adicional4_n`="+adicional4_n+",`adicional4_50`="+adicional4_50+", `adicional4_100`="+adicional4_100+", "+
